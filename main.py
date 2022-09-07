fc546c19289cedd9ded84495715a004997da550b
Fatina Shahzad <fatina.shahzad@attech-ltd.com>
2022-09-07 14:11:22 +0500

    added changes

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from math import sin
 import scipy.optimize
+
+
 def func(y):
-        x= 4*sin(y) - 4
+        x = 4*sin(y) - 4
         return x
-x= scipy.optimize.fsolve(func,0.3)
-print (x)
+
+
+x = scipy.optimize.fsolve(func, 0.3)
+print(x)
+print("hello, world!")
